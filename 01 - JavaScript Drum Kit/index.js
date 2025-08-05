246dd4a151dc2df94a3b12cc460f3fd5a08c1aa3
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-08-05 16:10:25 +0100

    Finished Javascript drumkit

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,25 @@
+// function removeTransition(e) {
+//     if (e.propertyName !== 'transform') return;
+//     e.target.classList.remove('playing');
+// }
+  
+function playSound(e) {
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    if (!audio) {
+        return;
+    }
+
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function endTransition(e) {
+    e.target.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", endTransition));
+window.addEventListener('keydown', playSound);
