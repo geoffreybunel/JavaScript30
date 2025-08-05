9dabc383db941c2a0d799be090c85b2ab19de2a4
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-08-05 16:53:01 +0100

    CSS + JS clock

---
+++ 02 - JS and CSS Clock/index.js
@@ -0,0 +1,22 @@
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".min-hand");
+const secondHand = document.querySelector(".second-hand");
+
+function showTime() {
+    const date = new Date();
+
+    const seconds = date.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360 + 90);
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = date.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = date.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(showTime, 1000);
+showTime();
